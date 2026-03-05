b45aa515b69c7e0fbd8f9fc3d78f0eeea2d78e7a
Matthias Kalms <mtkalms@gmail.com>
2026-03-05 07:39:34 -0800

    Add sketch for WinLoss

---
+++ src/rich_illuminations/winloss.py
@@ -0,0 +1,50 @@
+from typing import Optional, Sequence, Union
+
+from rich.color import Color
+from rich.console import ConsoleOptions, RenderResult, Console
+from rich.measure import Measurement
+from rich.segment import Segment
+from rich.style import Style
+
+from graphical._buckets import buckets, SummaryFunction
+from graphical.mark import Mark, BAR_BLOCK_V
+
+Numeric = Union[int, float]
+
+
+class WinLoss:
+    def __init__(
+        self,
+        values: Sequence[Numeric],
+        width: Optional[int] = None,
+        marks: Mark = BAR_BLOCK_V,
+        color: Optional[Color | str] = None,
+        negcolor: Optional[Color | str] = None,
+        bgcolor: Optional[Color | str] = None,
+        summary_function: SummaryFunction = sum,
+    ):
+        self.values = values
+        self.width = width or len(values)
+        self.marks = marks
+        self.color = color
+        self.negcolor = negcolor
+        self.bgcolor = bgcolor
+        self.summary_function = summary_function
+
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        pos_style = Style(color=self.color, bgcolor=self.bgcolor)
+        neg_style = Style(color=self.negcolor, bgcolor=self.bgcolor)
+        for value in buckets(self.values, self.width, self.summary_function):
+            if value > 0:
+                yield Segment(self.marks.get(-0.5), style=pos_style)
+            elif value < 0:
+                yield Segment(self.marks.get(0.5), style=neg_style)
+            else:
+                yield Segment(" ", style=pos_style)
+
+    def __rich_measure__(
+        self, console: Console, options: ConsoleOptions
+    ) -> Measurement:
+        return Measurement(self.width, 1)
