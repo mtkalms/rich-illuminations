4a8870e8d691aa70306d452c97fd1704ced5303a
Matthias Kalms <mtkalms@gmail.com>
2026-03-07 12:05:21 -0800

    Test: Clean up sparkline tests

--- tests/test_sparkline.py
+++ tests/test_sparkline.py
@@ -1,40 +1,57 @@
 import pytest
 
 from rich_illuminations import Sparkline
-from graphical.mark import BAR_BLOCK_V, BAR_SHADE, Mark
+from graphical.mark import BAR_BLOCK_V, BAR_SHADE, Mark, Tuple
 
 from tests.utilities.asserts import assert_markup
 
 
-class Test_Sparkline:
-    @pytest.mark.parametrize(
-        "cells, width, expected",
-        [
-            (BAR_BLOCK_V, 9, "  ▁▂▃▄▅▆▇██"),
-            (BAR_SHADE, 5, "  ░▒▓██"),
-        ],
-        ids=["AREA", "SHADE"],
+@pytest.mark.parametrize(
+    "cells, width, expected",
+    [
+        (BAR_BLOCK_V, 9, "  ▁▂▃▄▅▆▇██"),
+        (BAR_SHADE, 5, "  ░▒▓██"),
+    ],
+    ids=["AREA", "SHADE"],
+)
+def test_ascending(cells: Mark, width: int, expected: str):
+    chart = Sparkline(
+        values=[-1 + d for d in range(width + 2)],
+        value_range=(0, width - 1),
+        marks=cells,
     )
-    def test_ascending(self, cells: Mark, width: int, expected: str):
-        chart = Sparkline(
-            values=[-1 + d for d in range(width + 2)],
-            value_range=(0, width - 1),
-            marks=cells,
-        )
-        assert_markup(chart, expected)
+    assert_markup(chart, expected)
 
-    @pytest.mark.parametrize(
-        "cells, width, expected",
-        [
-            (BAR_BLOCK_V, 9, "███▇▆▅▄▃▂▁ "),
-            (BAR_SHADE, 5, "███▓▒░ "),
-        ],
-        ids=["AREA", "SHADE"],
+
+@pytest.mark.parametrize(
+    "cells, width, expected",
+    [
+        (BAR_BLOCK_V, 9, "███▇▆▅▄▃▂▁ "),
+        (BAR_SHADE, 5, "███▓▒░ "),
+    ],
+    ids=["AREA", "SHADE"],
+)
+def test_descending(cells: Mark, width: int, expected: str):
+    chart = Sparkline(
+        values=[width + 1 - d for d in range(width + 2)],
+        value_range=(0, width - 1),
+        marks=cells,
     )
-    def test_descending(self, cells: Mark, width: int, expected: str):
-        chart = Sparkline(
-            values=[width + 1 - d for d in range(width + 2)],
-            value_range=(0, width - 1),
-            marks=cells,
-        )
-        assert_markup(chart, expected)
+    assert_markup(chart, expected)
+
+
+@pytest.mark.parametrize(
+    "value, value_range, expected",
+    [
+        (12, (0, 10), "█"),
+        (-2, (0, 10), " "),
+    ],
+    ids=["over", "under"],
+)
+def test_out_of_bounds(value: float, value_range: Tuple[float, float], expected: str):
+    chart = Sparkline(
+        values=[value],
+        value_range=value_range,
+        marks=BAR_BLOCK_V,
+    )
+    assert_markup(chart, expected)
