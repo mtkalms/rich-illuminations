089f845c4bad6571a2b1914737c1b17c8df58602
Matthias Kalms <mtkalms@gmail.com>
2026-03-08 15:02:07 -0700

    Test: Add simple tests for Horizon

---
+++ tests/test_horizon.py
@@ -0,0 +1,28 @@
+from typing import Sequence
+
+from graphical.utils import Numeric
+import pytest
+
+from rich_illuminations import Horizon
+from tests.utilities.asserts import assert_markup
+
+
+@pytest.mark.parametrize(
+    "values, expected",
+    [
+        (
+            [-5 + d for d in range(11)],
+            "[#4d9221] ▂▅▇[/][#7fbc41 on #4d9221]▂▄▆[/][#b8e186 on #7fbc41]▁▃▆█[/]",
+        ),
+        (
+            [5 - d for d in range(11)],
+            "[#b8e186 on #7fbc41]█▆▃▁[/][#7fbc41 on #4d9221]▆▄▂[/][#4d9221]▇▅▂ [/]",
+        ),
+    ],
+    ids=["ascending", "descending"],
+)
+def test_values(values: Sequence[Numeric], expected: str):
+    chart = Horizon(
+        values=values, value_range=(-5, 5), colors=["#4d9221", "#7fbc41", "#b8e186"]
+    )
+    assert_markup(chart, expected, preview=True)
