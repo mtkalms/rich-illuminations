3aeaf23698ef7bd4d88ab037b343104f3689b1a1
Matthias Kalms <mtkalms@gmail.com>
2026-03-13 09:48:33 -0700

    Add Bar and Stack to module

--- src/rich_illuminations/__init__.py
+++ src/rich_illuminations/__init__.py
@@ -1,12 +1,16 @@
+from ._bar import Bar
 from ._bullet import Bullet
 from ._horizon import Horizon
 from ._sparkline import Sparkline
+from ._stack import Stack
 from ._winloss import WinLoss
 
 
 __all__ = [
+    "Bar",
     "Bullet",
     "Horizon",
     "Sparkline",
+    "Stack",
     "WinLoss",
 ]
